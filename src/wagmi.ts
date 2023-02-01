df7f07f36b84d9e35534ebff8b697569e3c51cef
moxey.eth <jakemoxey@gmail.com>
2023-02-01 19:59:52 +1100

    add ledger

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,25 +1,31 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createClient } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
+import { connectorsForWallets, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { ledgerWallet } from '@rainbow-me/rainbowkit/wallets';
+import { configureChains, createClient } from 'wagmi';
+import { goerli, mainnet } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider, webSocketProvider } = configureChains(
   [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
-  [
-    publicProvider(),
-  ],
-)
+  [publicProvider()]
+);
 
-const { connectors } = getDefaultWallets({
+const { wallets } = getDefaultWallets({
   appName: 'My wagmi + RainbowKit App',
   chains,
-})
+});
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [ledgerWallet({ chains })],
+  },
+]);
 
 export const client = createClient({
   autoConnect: true,
   connectors,
   provider,
   webSocketProvider,
-})
+});
 
-export { chains }
+export { chains };
